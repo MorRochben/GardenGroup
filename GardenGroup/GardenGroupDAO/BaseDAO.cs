59c9dcf25b6f20ff8c6ce48b032003f65d806c4c
Mayron van Leenden <mvanleenden@gmail.com>
2020-09-20 20:37:33 +0200

    CRUD - Ticket CRUD - User Connected to Mongo using Singleton

---
+++ GardenGroup/GardenGroupDAO/BaseDAO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GardenGroupDAO
+{
+    public abstract class BaseDAO
+    {
+        protected readonly MongoDB db;
+
+        public BaseDAO()
+        {
+            db = MongoDB.Instance;
+        }
+    }
+}
